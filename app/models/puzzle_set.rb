6d9b92df12d6ed8f55b66c25d486a4170e974ba6
Ubuntu <ubuntu@ip-10-0-0-16.ec2.internal>
2017-04-21 20:53:43 +0000

    Fixed the query_dictionary method to alphabetize

--- app/models/puzzle_set.rb
+++ app/models/puzzle_set.rb
@@ -54,6 +54,10 @@ class PuzzleSet < ActiveRecord::Base
       # Attach solution
       solutions.push( [candidateWord, score] )
     end
+    
+    # Sort the solutions, first by score then alphabetize the words
+    solutions = solutions.sort{|a,b| (a[1] <=> b[1]) == 0 ? (a[0] <=> b[0]) : (b[1] <=> a[1]) }
+
     return solutions
   end
 
