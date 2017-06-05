31a8151851ff686fe7a94d787e13b8c50c210e06
Ubuntu <ubuntu@ip-10-0-0-16.ec2.internal>
2017-06-05 23:38:55 +0000

    Fixed some UI/UX with JS for some scrolling, and zeroing out of some divs to make more intuitive.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,4 +1,8 @@
 function populateSolveForm(firstChar, otherChars) {
+  $("#puzzleSet_target").html("");
   $("#puzzle_set_center_letter").val(firstChar);
   $("#puzzle_set_other_letters").val(otherChars);
+  $('html, body').animate({
+    scrollTop: $("#puzzle_set_center_letter").offset().top
+    }, 600);
 }
